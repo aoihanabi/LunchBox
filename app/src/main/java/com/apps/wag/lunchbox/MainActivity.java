1abda452d34ad52c654ba30142ad6d4ee80338f4
aoihanabi <abii98cm@gmail.com>
2019-03-14 00:10:33 -0600

    1. Botón "My Recipebook" lleva a otra activity (Relative Layout on xml and some java code for the button) 2. ListView muestra elementos del ArrayList usando un ArrayAdapter

--- app/src/main/java/com/apps/wag/lunchbox/MainActivity.java
+++ app/src/main/java/com/apps/wag/lunchbox/MainActivity.java
@@ -1,13 +1,29 @@
 package com.apps.wag.lunchbox;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button goRecipebook;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        goRecipebook = (Button) findViewById(R.id.btnGoRecipeBook);
+        goRecipebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRecipeBook();
+            }
+        });
+    }
+
+    private void openRecipeBook() {
+        Intent inten = new Intent(this, MyRecipeBook.class);
+        startActivity(inten);
     }
 }
--- app/src/main/java/com/apps/wag/lunchbox/MyRecipeBook.java
+++ app/src/main/java/com/apps/wag/lunchbox/MyRecipeBook.java
@@ -2,12 +2,31 @@ package com.apps.wag.lunchbox;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MyRecipeBook extends AppCompatActivity {
+    ListView listita;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_recipe_book);
+
+        listita = (ListView) findViewById(R.id.listViewId);
+        ArrayList <String> listaNombres = new ArrayList<>();
+        listaNombres.add("Receta 0");
+        listaNombres.add("Receta 1");
+        listaNombres.add("Receta 2");
+        listaNombres.add("Receta 3");
+        listaNombres.add("Receta 4");
+        listaNombres.add("Receta 5");
+        listaNombres.add("Receta 6");
+        listaNombres.add("Receta 7");
+
+        ArrayAdapter adaptador = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listaNombres);
+        listita.setAdapter(adaptador);
     }
 }
